a964360b1ceb6c0fb0ae6fcef8aae38662cd77a1
Yasmin Gomes <yasmingfabian@gmail.com>
2026-04-29 07:52:47 -0300

    Refactor chess piece movement logic in xadrez.c

--- xadrez.c
+++ xadrez.c
@@ -1,29 +1,35 @@
 #include <stdio.h>
 
 int main() {
-    // Torre - 5 casas direita (for)
-    printf("TORRE (Direita x5):\n");
-    for(int i = 0; i < 5; i++) {
+    int torre = 0;
+    int bispo = 0;
+    int rainha = 0;
+
+    printf("Quantidade de casas que a peça Torre vai se mover para direita:\n");
+    scanf("%d", &torre);
+    for(int i = 0; i < torre; i++) {
         printf("Direita\n");
     }
     printf("\n");
 
     // Bispo - 5 casas diagonal (while)
-    printf("BISPO (Diagonal x5):\n");
+    printf("Quantidade de casas que a peça Bispo vai se mover para cima e direita:\n");
+    scanf("%d", &bispo);
     int b = 0;
-    while(b < 5) {
+    while(b < bispo) {
         printf("Cima, Direita\n");
         b++;
     }
     printf("\n");
 
     // Rainha - 8 casas esquerda (do-while)
-    printf("RAINHA (Esquerda x8):\n");
+    printf("Quantidade de casas que a peça Rainha vai se mover para esquerda:\n");
+    scanf("%d", &rainha);
     int r = 0;
     do {
         printf("Esquerda\n");
         r++;
-    } while(r < 8);
+    } while(r < rainha);
 
     return 0;
 }
